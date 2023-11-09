32231b0a77fe51ba08d5ada34d232c17d9918b8f
Jae Kim <jaekim0519@gmail.com>
2023-11-09 11:27:09 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import express from "express";
+import Lab5 from "./lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import cors from "cors";
+
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+
+app.use(express.json());
+
+CourseRoutes(app);
+ModuleRoutes(app);
+Lab5(app);
+
+app.listen(process.env.PORT || 4000);
